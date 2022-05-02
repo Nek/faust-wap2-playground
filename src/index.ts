193904dd2963995fac679eda4162d55fa9840766
Nikita Dudnik <nikdudnik@gmail.com>
2022-05-02 05:43:27 +0200

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@ import loadPlugin from "./loadPlugin"
 import spectrometer from "./spectrometer"
 import oscilloscope from "./oscilloscope"
 
-var ctx = new AudioContext()
+const ctx = new AudioContext()
 
 const FFT_SIZE = 1024
-
-var splitter = ctx.createChannelSplitter(3)
-var analyser = ctx.createAnalyser()
+const analyser = ctx.createAnalyser()
 analyser.fftSize = FFT_SIZE
 
 loadPlugin(ctx, "./melody.wasm").then((node) => {
@@ -15,7 +13,6 @@ loadPlugin(ctx, "./melody.wasm").then((node) => {
     node.setOutputParamHandler((path: string, value: number | undefined) =>
       console.log(path, value)
     )
-    node.connect(splitter)
     node.connect(analyser)
     node.connect(ctx.destination)
     spectrometer(
