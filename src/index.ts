3763f65925272a33b302399e9881a54b360d6e58
Nikita Dudnik <nikdudnik@gmail.com>
2022-05-02 05:39:31 +0200

    Add AudioContext resume fix

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,24 @@ loadPlugin(ctx, "./melody.wasm").then((node) => {
     node.connect(splitter)
     node.connect(analyser)
     node.connect(ctx.destination)
-    spectrometer(analyser, document.getElementById("spectrum") as HTMLCanvasElement)
-    oscilloscope(analyser, document.getElementById("oscilloscope") as HTMLCanvasElement)
+    spectrometer(
+      analyser,
+      document.getElementById("spectrum") as HTMLCanvasElement
+    )
+    oscilloscope(
+      analyser,
+      document.getElementById("oscilloscope") as HTMLCanvasElement
+    )
   }
 })
+
+const unlockAudioContext = (ctx: AudioContext) => {
+  if (ctx.state !== "suspended") return
+  const b = document.body
+  const events = ["touchstart", "touchend", "mousedown", "keydown"]
+  const unlock: () => void = () => ctx.resume().then(clean)
+  const clean: () => void = () => events.forEach((e) => b.removeEventListener(e, unlock))
+  events.forEach((e) => b.addEventListener(e, unlock, false))
+}
+
+unlockAudioContext(ctx)
