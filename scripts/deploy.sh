14d7317425e7bdcb9b96767976f923cbf919a144
Nikita Dudnik <nikdudnik@gmail.com>
2022-05-07 16:44:30 +0200

    Add deployment to netlify

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+rm -rf /tmp/dist;
+cp -r dist /tmp/dist;
+cd /tmp/dist;
+git init;
+git add -A;
+git commit -m "Deploy";
+git remote add origin git@github.com:Nek/faust-sandbox-deploy.git;
+git push origin master --force;
